a1cfa32d440153c73f310244d6306318b2211edd
chris <chrisjacob876@gmail.com>
2024-12-21 11:14:34 +0530

    created appoinment app and model

---
+++ appointments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=200)
+    phone_number = models.BooleanField(default=False)
+    date = models.DateTimeField("Appointment Date")
+    slot = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
