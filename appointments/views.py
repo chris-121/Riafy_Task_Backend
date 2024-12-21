cc54bcf471a82f7e4f7978d8156b243ce899b9f8
chris <chrisjacob876@gmail.com>
2024-12-21 11:17:47 +0530

    url setup

--- appointments/views.py
+++ appointments/views.py
@@ -1,1 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from .utils.exception_handlers import handle_exception
+from . import services
+
+
+@api_view(["GET", "POST"])
+def appointment(request):
+    try:
+        if request.method == "GET":
+            todos = services.get_all_todos()
+            return Response(todos)
+        elif request.method == "POST":
+            todo = services.add_todo(request.data)
+            return Response(todo, status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        error_object = handle_exception(e)
+        return Response({"Error": error_object["Error"]}, status=error_object["Status"])
--- riafy_task_backend/settings.py
+++ riafy_task_backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "appointments.apps.AppointmentsConfig",
     "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
