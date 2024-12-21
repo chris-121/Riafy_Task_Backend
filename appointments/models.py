0db65c8f17708f6bebd37281cbbfdd161a141b96
chris <chrisjacob876@gmail.com>
2024-12-21 11:27:11 +0530

    added serializers

--- appointments/models.py
+++ appointments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Appointment(models.Model):
     name = models.CharField(max_length=200)
-    phone_number = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15)
     date = models.DateTimeField("Appointment Date")
     slot = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
