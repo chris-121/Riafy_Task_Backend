87d27c80e1ff8386fe01898b7b81207f284643a8
chris <chrisjacob876@gmail.com>
2024-12-21 11:51:23 +0530

    services issue fixed

--- appointments/services.py
+++ appointments/services.py
@@ -1,9 +1,21 @@
-from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from .models import Appointment
 from .serializers import AppointmentSerializer
 
 
-def get_all_appointments():
-    todos = Appointment.objects.all()
-    serializer = AppointmentSerializer(todos, many=True)
+def get_all_appointments(date):
+    appointments = Appointment.objects.filter(date__date=date)
+    serializer = AppointmentSerializer(appointments, many=True)
     return serializer.data
+
+
+def create_appointment(appointment):
+    if not appointment or not isinstance(appointment, dict):
+        raise ValidationError("Invalid data type. Expected a dictionary.")
+
+    serializer = AppointmentSerializer(data=appointment)
+    if serializer.is_valid():
+        serializer.save()
+        return serializer.data
+    else:
+        raise ValidationError(serializer.errors)
